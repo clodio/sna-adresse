bfc3401aeee6e050c437ba2fec27fd5f299d3352
Claude Seguret <claude.seguret@laposte.fr>
2018-01-05 17:39:16 +0100

    Proposition site adresse

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
+# Smart address
 
 [![Build Status](https://travis-ci.org/clodio/sna-adresse.svg?branch=master)](https://travis-ci.org/clodio/sna-adresse)
-
--- js/creative.js
+++ js/creative.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -17,7 +17,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
@@ -61,4 +61,46 @@
         }
     });
 
+
+    function syntaxHighlight(json) {
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var jsonClassToApply = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    jsonClassToApply = 'key';
+                } else {
+                    jsonClassToApply = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                jsonClassToApply = 'boolean';
+            } else if (/null/.test(match)) {
+                jsonClassToApply = 'null';
+            }
+            return '<span class="json_' + jsonClassToApply + '">' + match + '</span>';
+        });
+    };
+
+
 })(jQuery); // End of use strict
+
+/*!***************************************************
+ * mark.js v8.11.0
+ * https://github.com/julmot/mark.js
+ * Copyright (c) 2014–2017, Julian Motz
+ * Released under the MIT license https://git.io/vwTVl
+ *****************************************************/
+
+"use strict";
+
+var _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+        for (var key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                target[key] = source[key];
+            }
+        }
+    }
+    return target;
+};
